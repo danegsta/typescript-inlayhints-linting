4c3880bec2f87000027838babd33761f0596e3ed
David Negstad <David.Negstad@microsoft.com>
2022-07-20 10:26:11 -0700

    Add updated second repro example

--- reduceRepro.ts
+++ reduceRepro.ts
@@ -1,7 +1,11 @@
 export function templateReduce(strings: TemplateStringsArray, ...expr: Array<string | null | undefined>): string {
     return expr.reduce<string>(
         (accumulator, currentExpr, index) => {
-            return accumulator + strings[index] + currentExpr;
+            if (!!currentExpr) {
+                return accumulator + strings[index] + currentExpr;
+            }
+
+            return accumulator;
         },
         '',
     ) + strings.slice(-1);
